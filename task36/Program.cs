0dd61edf04c568541f0054209b38624281cba094
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-26 15:45:40 +0300

    Sem005_DZ

---
+++ task36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+var array = FillArray(5);
+Print(array);
+Console.WriteLine();
+Console.WriteLine(GetSum(array));
+
+int GetSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + arr[i];
+        }
+    }
+    return sum;
+
+}
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item}, ");
+    }
+}
---
+++ task38/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+var array = FillArray(10);
+Print(array);
+Console.WriteLine();
+Console.WriteLine($"max = {GetMax(array)}");
+Console.WriteLine($"min = {GetMin(array)}");
+Console.WriteLine(GetDiff(array));
+
+double GetMax(double[] arr)
+{
+    int item = 0;
+    double max = arr[item];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+double GetMin(double[] arr)
+{
+    int item = 0;
+    double min = arr[item];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+double GetDiff(double[] arr)
+{
+    double dif = 0;
+    dif = GetMax(arr) - GetMin(arr);
+    return dif;
+}
+
+double[] FillArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-20, 20);
+    }
+    return array;
+}
+
+void Print(double[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item}, ");
+    }
+}
